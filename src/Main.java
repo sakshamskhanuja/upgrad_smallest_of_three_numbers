679e094b9a9acc8dc3d0486441b4890316967c2d
Saksham Singh Khanuja <sskhanuja94@gmail.com>
2023-01-09 02:43:20 +0530

    Updated logic

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,21 @@ public class Main {
         int n2 = scanner.nextInt();
         int n3 = scanner.nextInt();
 
-        // Stores the smallest number.
-        int small = 0;
-
-        // Checks if n1 is the smallest.
-        if (n1 < n2 && n1 < n3) {
-            small = n1;
+        // Checks if n1 is smaller than n2.
+        if (n1 < n2) {
+            // Checks if n1 is smaller than n3.
+            if (n1 < n3) {
+                System.out.println("The smallest number entered is " + n1);
+            } else {
+                System.out.println("The smallest number entered is " + n3);
+            }
+        } else {
+            // Checks n2 is smaller than n3.
+            if (n2 < n3) {
+                System.out.println("The smallest number entered is " + n2);
+            } else {
+                System.out.println("The smallest number entered is " + n3);
+            }
         }
-
-        // Checks if n2 is the smallest.
-        else if (n2 < n1 && n2 < n3) {
-            small = n2;
-        }
-
-        // Checks if n3 is the smallest.
-        else if (n3 < n1 && n3 < n2) {
-            small = n3;
-        }
-
-        // Prints the smallest number.
-        System.out.println("The smallest number entered is " + small);
     }
 }
